8bf1935bf0a484156082927ee758818c4f3f2eb3
Yonimx <128926179+Yonimx@users.noreply.github.com>
2023-05-13 23:41:06 +0800

    Add files via upload

--- lesson/script.js
+++ lesson/script.js
@@ -8,3 +8,14 @@
         icon.src = "moon.png"
     }
 }
+
+var icon2 = document.getElementById("icon2");
+
+icon.onclick = function(){
+document.body.classList.toggle("dark-theme");
+if(document.body.classList.contains("dark-theme")){
+    icon.src = "sun.png";
+}else{
+    icon.src = "moon.png"
+}
+}
