a6096f3d169fc45d0bd4ea7f84b1647db726dd8b
Yonimx <128926179+Yonimx@users.noreply.github.com>
2023-06-09 03:29:20 +0800

    Add files via upload

---
+++ qui/q23 copy 2/js/questions.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "According to the Aufbau principle, in which order are electrons filled in atomic orbitals?",
+    answer: "1s, 2s, 2p, 3s, 3p, 4s, 3d, 4p",
+    options: [
+      "1s, 2s, 2p, 3s, 3p, 4s, 3d, 4p",
+      "2s, 2p, 3s, 3p, 4s, 3d, 4p, 5s",
+      "1s, 2s, 2p, 3s, 3p, 4s, 4d, 5s",
+      "2s, 2p, 3s, 3p, 4s, 4d, 5s, 5p"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which principle states that a maximum of two electrons, each with opposite spins, can fit in an orbital?",
+    answer: "Pauli exclusion principle",
+    options: [
+      "Pauli exclusion principle",
+      "Hund's rule",
+      "Aufbau principle",
+      "Octet rule"
+    ]
+  },
+    {
+    numb: 3,
+    question: "According to Hund's rule, how are electrons filled in orbitals belonging to a subshell?",
+    answer: "Every orbital is singly occupied by electrons before a second electron is filled in an orbital.",
+    options: [
+      "Every orbital is singly occupied by electrons before a second electron is filled in an orbital.",
+      "Electrons fill orbitals in a random order within a subshell.",
+      "Electrons fill orbitals in increasing order of their energies.",
+      "Electrons fill orbitals in decreasing order of their energies."
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the electron configuration of hydrogen?",
+    answer: "1s1",
+    options: [
+      "1s1",
+      "1s2 2s2 2p4",
+      "1s2 2s2 2p4",
+      "[Ne]3s23p5"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many electrons are in the oxygen atom?",
+    answer: "8",
+    options: [
+      "4",
+      "6",
+      "8",
+      "17"
+    ]
+  },
+    { 
+    numb: 6,
+    question: "What is the electron configuration of oxygen?",
+    answer: "1s2 2s2 2p4",
+    options: [
+      "1s2 2s2 2p4",
+      "1s1",
+      "1s2 2s2 2p6",
+      "[Ne]3s23p5"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What is the electron configuration of chlorine?",
+    answer: "1s22s22p63s23p5",
+    options: [
+      "1s2 2s2 2p4",
+      "1s1",
+      "1s22s22p63s23p5",
+      "[Ne]3s23p5"
+    ]
+  },
+    { 
+    numb: 8,
+    question: "How many electrons are in the chlorine atom?",
+    answer: "17",
+    options: [
+      "2",
+      "6",
+      "7",
+      "17"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which principle states that electrons occupy orbitals with lower energy before occupying higher energy orbitals?",
+    answer: "Aufbau principle",
+    options: [
+      "Aufbau principle",
+      "Pauli exclusion principle",
+      "Hund's rule",
+      "Octet rule"
+    ]
+  },
+    { 
+    numb: 10,
+    question: "Which atomic orbitals are filled after the 4s subshell according to the Aufbau principle?",
+    answer: "3d",
+    options: [
+      "3d",
+      "5s",
+      "4p",
+      "6s"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Which is not true about amorphous solids?",
+    answer: "Anisotrophic",
+    options: [
+      "IMF are not equal",
+      "Irregular arrangement",
+      "Anisotrophic",
+      "Shapeless"
+    ]
+  },
+    { 
+    numb: 12,
+    question: "What is a force within a single molecule?",
+    answer: "Intramolecular Forces",
+    options: [
+      "Net Force",
+      "London Bridge Forces",
+      "Intramolecular Forces",
+      "Negative Force"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Strong Intermolecular forces but Low kinetic energy",
+    answer: "Solid",
+    options: [
+      "Solid",
+      "Water",
+      "amorphous",
+      "Gas"
+    ]
+  },
+    { 
+    numb: 14,
+    question: "Two or more substance are mixed together to form a solution",
+    answer: "Solution",
+    options: [
+      "Experiment",
+      "Goku",
+      "Solute",
+      "Solution"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What is the substance that dissolves solute?",
+    answer: "Solvent",
+    options: [
+      "Luffy",
+      "Solvent",
+      "Naruto",
+      "bolpen"
+    ]
+  },
+]
---
+++ qui/q23 copy/js/questions.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the maximum number of electrons that can be accommodated in the K shell?",
+    answer: "2",
+    options: [
+      "2",
+      "8",
+      "18",
+      "32"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which quantum number determines the subshells into which electrons are distributed?",
+    answer: "Azimuthal quantum number",
+    options: [
+      "Principal quantum number",
+      "Azimuthal quantum number",
+      "Magnetic quantum number",
+      "Spin quantum number"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many electrons can the p subshell accommodate?",
+    answer: "6",
+    options: [
+      "2",
+      "6",
+      "10",
+      "14"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which subshell corresponds to l = 2 in the electron configuration?",
+    answer: "d subshell",
+    options: [
+      "s subshell",
+      "p subshell",
+      "d subshell",
+      "f subshell"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the electron configuration notation for an atom with two electrons in the 1s subshell?",
+    answer: "1s2",
+    options: [
+      "1s2",
+      "2s2",
+      "2p6",
+      "3s2"
+    ]
+  },
+    { 
+    numb: 6,
+    question: "How many electrons can the 3d subshell accommodate?",
+    answer: "10",
+    options: [
+      "2",
+      "6",
+      "10",
+      "14"
+    ]
+  },
+    {
+    numb: 7,
+    question: "How many electrons can the 3d subshell accommodate?",
+    answer: "f subshell",
+    options: [
+      "s subshell",
+      "p subshell",
+      "d subshell",
+      "f subshell"
+    ]
+  },
+    { 
+    numb: 8,
+    question: "What is the maximum number of electrons that can be accommodated in the N shell?",
+    answer: "32",
+    options: [
+      "2",
+      "8",
+      "18",
+      "32"
+    ]
+  },
+    {
+    numb: 9,
+    question: "How many electrons are in the 2p subshell?",
+    answer: "6",
+    options: [
+      "2",
+      "6",
+      "10",
+      "14"
+    ]
+  },
+    { 
+    numb: 10,
+    question: "What is the electron configuration notation for magnesium (atomic number 12)?",
+    answer: "1s2 2s2 2p6 3s2",
+    options: [
+      "What is the electron configuration notation for magnesium (atomic number 12)?",
+      "2s2 2p6 3s2",
+      "1s2 2s2 2p6",
+      "3s2"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Which is not true about amorphous solids?",
+    answer: "Anisotrophic",
+    options: [
+      "IMF are not equal",
+      "Irregular arrangement",
+      "Anisotrophic",
+      "Shapeless"
+    ]
+  },
+    { 
+    numb: 12,
+    question: "What is a force within a single molecule?",
+    answer: "Intramolecular Forces",
+    options: [
+      "Net Force",
+      "London Bridge Forces",
+      "Intramolecular Forces",
+      "Negative Force"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Strong Intermolecular forces but Low kinetic energy",
+    answer: "Solid",
+    options: [
+      "Solid",
+      "Water",
+      "amorphous",
+      "Gas"
+    ]
+  },
+    { 
+    numb: 14,
+    question: "Two or more substance are mixed together to form a solution",
+    answer: "Solution",
+    options: [
+      "Experiment",
+      "Goku",
+      "Solute",
+      "Solution"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What is the substance that dissolves solute?",
+    answer: "Solvent",
+    options: [
+      "Luffy",
+      "Solvent",
+      "Naruto",
+      "bolpen"
+    ]
+  },
+]
